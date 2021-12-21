b909b3cec4799f4d701e816c7900feeded593150
Luiz Henrique Santos <luiz.santos85@gmail.com>
2021-12-20 22:54:50 -0300

    criaçao do models fake e auxiliares

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,34 +1,39 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {
-   res.render('pages/page',{
-       banner:{
-           title:'Todos os animais',
-           bckg: 'allanimals.jpg'
-       }
+   res.render('pages/page', {
+      menu: createMenuObject('all'),
+      banner: {
+         title: 'Todos os animais',
+         bckg: 'allanimals.jpg',
+      },
    });
 };
 export const dogs = (req: Request, res: Response) => {
-   res.render('pages/page',{
-       banner:{
-           title:'Cachorros',
-           bckg: 'banner_dog.jpg'
-       }
+   res.render('pages/page', {
+      menu: createMenuObject('dog'),
+      banner: {
+         title: 'Cachorros',
+         bckg: 'banner_dog.jpg',
+      },
    });
 };
 export const cats = (req: Request, res: Response) => {
-   res.render('pages/page',{
-       banner:{
-           title:'Gatos',
-           bckg: 'banner_cat.jpg'
-       }
+   res.render('pages/page', {
+      menu: createMenuObject('cat'),
+      banner: {
+         title: 'Gatos',
+         bckg: 'banner_cat.jpg',
+      },
    });
 };
 export const fishes = (req: Request, res: Response) => {
-   res.render('pages/page',{
-       banner:{
-           title:'Peixes',
-           bckg: 'banner_fish.jpg'
-       }
+   res.render('pages/page', {
+      menu: createMenuObject('fish'),
+      banner: {
+         title: 'Peixes',
+         bckg: 'banner_fish.jpg',
+      },
    });
 };
