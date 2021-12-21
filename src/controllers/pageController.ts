dbe3f2dede3df834b3420fb0aa880956e16798a1
Luiz Henrique Santos <luiz.santos85@gmail.com>
2021-12-20 21:43:34 -0300

    ajustes de layouts das paginas

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,15 +1,34 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-   //res.render('pages/page');
-   res.send('Home no controller');
+   res.render('pages/page',{
+       banner:{
+           title:'Todos os animais',
+           bckg: 'allanimals.jpg'
+       }
+   });
 };
 export const dogs = (req: Request, res: Response) => {
-   //res.render('pages/page');
+   res.render('pages/page',{
+       banner:{
+           title:'Cachorros',
+           bckg: 'banner_dog.jpg'
+       }
+   });
 };
 export const cats = (req: Request, res: Response) => {
-   //res.render('pages/page');
+   res.render('pages/page',{
+       banner:{
+           title:'Gatos',
+           bckg: 'banner_cat.jpg'
+       }
+   });
 };
 export const fishes = (req: Request, res: Response) => {
-   //res.render('pages/page');
+   res.render('pages/page',{
+       banner:{
+           title:'Peixes',
+           bckg: 'banner_fish.jpg'
+       }
+   });
 };
